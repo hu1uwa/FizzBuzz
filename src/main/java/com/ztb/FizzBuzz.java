890ef573c9f367724de6ca810577f8cc9df21659
hu1uwa <250991112@qq.com>
2017-08-10 00:52:57 +0800

    should_equals_1 pass

---
+++ src/main/java/com/ztb/FizzBuzz.java
@@ -0,0 +1,19 @@
+package com.ztb;
+
+/**
+ * Created by z on 2017/8/10.
+ */
+public class FizzBuzz {
+    int firstNum;
+    int secondNum;
+    int thirdNum;
+    public void setFirstnum(int first, int second, int third) {
+        this.firstNum = first;
+        this.secondNum = second;
+        this.thirdNum = third;
+    }
+
+    public String play(int num) {
+       return num+"";
+    }
+}
---
+++ src/test/java/com/ztb/FizzBuzzTest.java
@@ -0,0 +1,23 @@
+package com.ztb;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+/**
+ * Created by z on 2017/8/10.
+ */
+public class FizzBuzzTest {
+    FizzBuzz fizzBuzz;
+    @Before
+    public void setUp() throws Exception {
+        fizzBuzz = new FizzBuzz();
+        fizzBuzz.setFirstnum(3,5,7);
+    }
+
+    @Test
+    public void should_equals_1() throws Exception {
+        assertEquals("1",fizzBuzz.play(1));
+    }
+}
